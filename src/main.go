4a24f9ab81d41a8cd7bf050e08a20a38a1a41a19
S-Maciejewski <sebastian_maciejewski@yahoo.com>
2020-08-16 14:28:07 +0200

    Test file upload

--- src/main.go
+++ src/main.go
@@ -1,20 +1,45 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
 
 func setupRouter(router *mux.Router) {
 	router.Methods("GET").Path("/test").HandlerFunc(testGet)
+	router.Methods("POST").Path("/testFile").HandlerFunc(testPostFile)
 }
 
 func testGet(w http.ResponseWriter, r *http.Request) {
-	log.Println("test get called")
+	log.Println("Test GET called")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Test"))
+}
+
+func testPostFile(w http.ResponseWriter, r *http.Request) {
+	log.Println("Test POST file request called")
+	// Limit max memory for a single file
+	r.ParseMultipartForm(32 << 20)
+	var buf bytes.Buffer
+	file, header, err := r.FormFile("file")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	name := strings.Split(header.Filename, ".")
+	fmt.Printf("File name %s\n", name[0])
+	io.Copy(&buf, file)
+
+	content := buf.String()
+	fmt.Println(content)
+	buf.Reset()
+	return
 }
 
 func main() {
